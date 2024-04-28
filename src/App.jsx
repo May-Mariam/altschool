09535eb323d7dd69ed3e53dd800ce02cff9869e2
*Adenekan <*mariammadenekan@gmail.com*>
2024-04-28 01:13:08 +0100

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GithubProjects from './github-projects';
 import ProjectDetails from './project-details';
+import NotFound from './404notfound';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<GithubProjects />} />
         <Route path="/project/:id" element={<ProjectDetails />} />
+        <Route path="*" element={<NotFound />} /> 
       </Routes>
     </BrowserRouter>
   );
